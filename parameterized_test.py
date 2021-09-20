f1cd97c4d2fbb6278234cd90b094c7413276758e
some random idiot <sittipat2554aa@hotmail.com>
2021-09-20 11:40:14 +0700

    Added parameterized_tests.py, which contains parameterized tests.

---
+++ parameterized_test.py
@@ -0,0 +1,42 @@
+import unittest
+from triangle import is_triangle
+
+
+class TriangleTest(unittest.TestCase):
+    valid_triangles = [(1, 1, 1),
+                       (3, 4, 5),
+                       (3, 4, 6),
+                       (8, 10, 12),
+                       (100, 101, 200),
+                       (0.9, 1.0, 1.1)]
+    not_triangles = [(21, 10, 10),
+                     (2, 1, 1),
+                     (6, 10, 4),
+                     (6, 20, 4),
+                     (5, 3, 10)]
+    invalid_arguments = [(-1, 2, 2),
+                         (1, 0, 2),
+                         (1, -1, 2),
+                         (1, 2, -1),
+                         (1, 2, 0),
+                         (-1, -1, -1),
+                         (0, 0, 0)]
+
+    def test_valid_triangle(self):
+        for a, b, c in self.valid_triangles:
+            with self.subTest():
+                msg = f"Side lengths ({a},{b},{c})"
+                self.assertTrue(is_triangle(a, b, c), msg)
+
+    def test_not_triangle(self):
+        for a, b, c in self.not_triangles:
+            with self.subTest():
+                msg = f"Side lengths ({a},{b},{c})"
+                self.assertFalse(is_triangle(a, b, c), msg)
+
+    def test_invalid_argument_raises_exception(self):
+        """Any non-positive argument should raise ValueError"""
+        for a, b, c in self.invalid_arguments:
+            with self.subTest():
+                msg = f"Side lengths ({a},{b},{c})"
+                self.assertRaises(ValueError, is_triangle, a, b, c)
